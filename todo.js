c9434775a0668fe703ad46405eb17ed7fe10366a
Gnoel01 <Gerthonoel@gmail.com>
2024-02-21 11:59:52 -0600

    Add to list

--- todo.js
+++ todo.js
@@ -1,13 +1,6 @@
 console.log("Hello, this is petco");
-var theTasks = [
-  "WELCOME TO THE TO-DO LIST!",
-  "PLEASE ENTER A TASK"
-];
-
-function addTextToPage(){
-
-
-}
+var theTasks = [];
+var index = 0;
 
 function addTask(){
   console.log(theTasks);
@@ -18,5 +11,31 @@ function addTask(){
   console.log("ADDING:", value1);
   theTasks.push(value1);
   console.log(theTasks);
-
+  var olList = addTextToPage(theTasks);
+  var divElement  = document.getElementById("list");
+  divElement.appendChild(olList);
 }
+
+function addTextToPage(arry){
+ 
+ // Create the list element:
+ var list = document.createElement('ol');
+
+ for (var i = 0; i < arry.length; i++) {
+     // Create the list item:
+     console.log("Adding to list..", arry[i]);
+     var item = document.createElement('li');
+
+     // Set its contents:
+     item.appendChild(document.createTextNode(arry[i]));
+
+     // Add it to the list:
+     list.appendChild(item);
+ }
+
+ // Finally, return the constructed list:
+ return list;
+}
+
+
+
