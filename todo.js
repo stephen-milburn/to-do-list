e3263cefa32c16e19a86d7bc506e73424d30afc1
jbeyer15 <masterchef325@yahoo.com>
2024-02-21 09:54:51 -0700

    ADDED ARRAY AND INPUT

--- todo.js
+++ todo.js
@@ -1,1 +1,22 @@
 console.log("Hello, this is petco");
+var theTasks = [
+  "WELCOME TO THE TO-DO LIST!",
+  "PLEASE ENTER A TASK"
+];
+
+function addTextToPage(){
+
+
+}
+
+function addTask(){
+  console.log(theTasks);
+  console.log("BEGGINING TASK ADD");
+
+  var input1 = document.getElementById("newTask");
+  var value1 = input1.value;
+  console.log("ADDING:", value1);
+  theTasks.push(value1);
+  console.log(theTasks);
+
+}
